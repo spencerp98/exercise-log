e3f8e70f879f0fa7a4a9c8daa7fe568d6670f35b
Patrick Spencer <spencerp@oregonstate.edu>
2017-12-20 15:44:56 -0500

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_spencerp',
-    password        : '1278',
+    password        : 'xxxx',
     database        : 'cs340_spencerp'
 });
 
